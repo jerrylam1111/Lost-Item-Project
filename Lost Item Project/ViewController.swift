264d244e193079ad6e3ab1c679209f2450eea72d
Jerry Lam <jerrylam1111@gmail.com>
2022-09-12 15:35:22 +1000

    Initial Commit

---
+++ Lost Item Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lost Item Project
+//
+//  Created by Jerry Chi Chong Lam on 12/9/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
